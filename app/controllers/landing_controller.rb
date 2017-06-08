0e9d75565bfcab6250a24902f7d2663e04c1a90a
mocivnik10 <iztok.mocivnik@poviolabs.com>
2017-06-08 16:52:46 +0200

    Articles
    - Style fixes on admin
    - Show articles on landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,8 @@
 class LandingController < ApplicationController
   def index
     @articles = Article.order('created_at').limit(3)
+    @testimonials = Testimonial.all
+    @first_three_testimonials = Testimonial.order('created_at').limit(3)
+    @second_three_testimonials = Testimonial.order('created_at').limit(6).offset(3)
   end
 end
