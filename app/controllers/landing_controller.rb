06bf88b001a26f5c2549173277954ff5c3115d14
mocivnik10 <iztok.mocivnik@poviolabs.com>
2017-06-22 13:26:07 +0200

    Fixes for landing page & footer

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,7 @@
 class LandingController < ApplicationController
   def index
     @articles = Article.order('created_at').limit(3)
-    @testimonials = Testimonial.all
-    @first_three_testimonials = Testimonial.order('created_at').limit(3)
-    @second_three_testimonials = Testimonial.order('created_at').limit(6).offset(3)
+    @testimonials = Testimonial.order('created_at').limit(6)
     @ipm_suggestions = IpmSuggestion.order('created_at').limit(6)
   end
 end
