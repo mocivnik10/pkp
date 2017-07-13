c82b2024df0bd70f0d9413eb6baf2bcf83c0bb99
mocivnik10 <iztok.mocivnik@poviolabs.com>
2017-07-13 09:28:23 +0200

    Minor fixes

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,7 @@
 class LandingController < ApplicationController
   def index
     @articles = Article.order('created_at').limit(3)
-    @testimonials = Testimonial.order('created_at').limit(6)
+    @testimonials = Testimonial.order('created_at').limit(9)
     @ipm_suggestions = IpmSuggestion.order('created_at').limit(6)
   end
 end
