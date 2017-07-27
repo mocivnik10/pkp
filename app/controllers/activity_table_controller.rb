21a9b9576351c8cf928d443a6ab9334fe8762beb
mocivnik10 <iztok.mocivnik@poviolabs.com>
2017-07-27 10:56:31 +0200

    Ipr tad
    - Changed links in navbar
    - Deleted activity_table controller and views
    - Joined activity_table with ipr tad usage
    - Fixed landing page ipr tad section

--- app/controllers/activity_table_controller.rb
+++
@@ -1,4 +0,0 @@
-class ActivityTableController < ApplicationController
-  def index
-  end
-end
--- test/controllers/activity_table_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ActivityTableControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get activity_table_index_url
-    assert_response :success
-  end
-
-end
